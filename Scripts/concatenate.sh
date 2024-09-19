c1d3f85754a71a547f0231e6d6a847ed061f7c4f
biol726314 <alr8519@utulsa.edu>
2024-09-19 15:33:04 -0500

    added concatenate sh and sbatch

---
+++ Scripts/concatenate.sh
@@ -0,0 +1,23 @@
+cat /scratch/biol726314/research/TrimmedLongReads/pc_bc01/*fastqsanger.gz>/scratch/biol726314/research/MergedLongReads/bc01_long.fastq.gz
+
+cat /scratch/biol726314/research/TrimmedLongReads/pc_bc02/*fastqsanger.gz>/scratch/biol726314/research/MergedLongReads/bc02_long.fastq.gz
+
+cat /scratch/biol726314/research/TrimmedLongReads/pc_bc03/*fastqsanger.gz>/scratch/biol726314/research/MergedLongReads/bc03_long.fastq.gz
+
+cat /scratch/biol726314/research/TrimmedLongReads/pc_bc04/*fastqsanger.gz>/scratch/biol726314/research/MergedLongReads/bc04_long.fastq.gz
+
+cat /scratch/biol726314/research/TrimmedLongReads/pc_bc05/*fastqsanger.gz>/scratch/biol726314/research/MergedLongReads/bc05_long.fastq.gz
+
+cat /scratch/biol726314/research/TrimmedLongReads/pc_bc06/*fastqsanger.gz>/scratch/biol726314/research/MergedLongReads/bc06_long.fastq.gz
+
+cat /scratch/biol726314/research/TrimmedLongReads/pc_bc07/*fastqsanger.gz>/scratch/biol726314/research/MergedLongReads/bc07_long.fastq.gz
+
+cat /scratch/biol726314/research/TrimmedLongReads/pc_bc08/*fastqsanger.gz>/scratch/biol726314/research/MergedLongReads/bc08_long.fastq.gz
+
+cat /scratch/biol726314/research/TrimmedLongReads/pc_bc09/*fastqsanger.gz>/scratch/biol726314/research/MergedLongReads/bc09_long.fastq.gz
+
+cat /scratch/biol726314/research/TrimmedLongReads/pc_bc10/*fastqsanger.gz>/scratch/biol726314/research/MergedLongReads/bc10_long.fastq.gz
+
+cat /scratch/biol726314/research/TrimmedLongReads/pc_bc11/*fastqsanger.gz>/scratch/biol726314/research/MergedLongReads/bc11_long.fastq.gz
+
+cat /scratch/biol726314/research/TrimmedLongReads/pc_bc12/*fastqsanger.gz>/scratch/biol726314/research/MergedLongReads/bc12_long.fastq.gz
