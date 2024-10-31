c590747e4f59a12cb825686924370797e71d5306
biol726314 <alr8519@utulsa.edu>
2024-10-31 16:11:15 -0500

    added rest of unicycler scritps

---
+++ Scripts/5thru9unicycler.sh
@@ -0,0 +1,14 @@
+#5
+unicycler -1 /home/ashleyrosb/TrimmedShort/5_S5_forward_paired.fq.gz -2 /home/ashleyrosb/TrimmedShort/5_S5_reverse_paired.fq.gz -l /home/ashleyrosb/TrimmedLong/MergedLong/mergedbc05.fastq.gz -o /scratch/ashleyrosb/Unicyc_out/uni_bc05
+
+#6
+unicycler -1 /home/ashleyrosb/TrimmedShort/6_S6_forward_paired.fq.gz -2 /home/ashleyrosb/TrimmedShort/6_S6_reverse_paired.fq.gz -l /home/ashleyrosb/TrimmedLong/MergedLong/mergedbc06.fastq.gz -o /scratch/ashleyrosb/Unicyc_out/uni_bc06
+
+#7
+unicycler -1 /home/ashleyrosb/TrimmedShort/7_S7_forward_paired.fq.gz -2 /home/ashleyrosb/TrimmedShort/7_S7_reverse_paired.fq.gz -l /home/ashleyrosb/TrimmedLong/MergedLong/mergedbc07.fastq.gz -o /scratch/ashleyrosb/Unicyc_out/uni_bc07
+
+#8
+unicycler -1 /home/ashleyrosb/TrimmedShort/8_S8_forward_paired.fq.gz -2 /home/ashleyrosb/TrimmedShort/8_S8_reverse_paired.fq.gz -l /home/ashleyrosb/TrimmedLong/MergedLong/mergedbc08.fastq.gz -o /scratch/ashleyrosb/Unicyc_out/uni_bc08
+
+#9
+unicycler -1 /home/ashleyrosb/TrimmedShort/9_S9_forward_paired.fq.gz -2 /home/ashleyrosb/TrimmedShort/9_S9_reverse_paired.fq.gz -l /home/ashleyrosb/TrimmedLong/MergedLong/mergedbc09.fastq.gz -o /scratch/ashleyrosb/Unicyc_out/uni_bc09
---
+++ Scripts/workingunicycler.sh
@@ -0,0 +1,20 @@
+#1
+unicycler -1 /home/ashleyrosb/TrimmedShort/1_S1_forward_paired.fq.gz -2 /home/ashleyrosb/TrimmedShort/1_S1_reverse_paired.fq.gz -l /home/ashleyrosb/TrimmedLong/MergedLong/mergedbc01.fastq.gz -o /scratch/ashleyrosb/Unicyc_out/uni_bc01
+
+#2
+unicycler -1 /home/ashleyrosb/TrimmedShort/2_S2_forward_paired.fq.gz -2 /home/ashleyrosb/TrimmedShort/2_S2_reverse_paired.fq.gz -l /home/ashleyrosb/TrimmedLong/MergedLong/mergedbc02.fastq.gz -o /scratch/ashleyrosb/Unicyc_out/uni_bc02
+
+#3
+unicycler -1 /home/ashleyrosb/TrimmedShort/3_S3_forward_paired.fq.gz -2 /home/ashleyrosb/TrimmedShort/3_S3_reverse_paired.fq.gz -l /home/ashleyrosb/TrimmedLong/MergedLong/mergedbc03.fastq.gz -o /scratch/ashleyrosb/Unicyc_out/uni_bc03
+
+#4
+unicycler -1 /home/ashleyrosb/TrimmedShort/4_S4_forward_paired.fq.gz -2 /home/ashleyrosb/TrimmedShort/4_S4_reverse_paired.fq.gz -l /home/ashleyrosb/TrimmedLong/MergedLong/mergedbc04.fastq.gz -o /scratch/ashleyrosb/Unicyc_out/uni_bc04
+
+#10
+unicycler -1 /home/ashleyrosb/TrimmedShort/10_S10_forward_paired.fq.gz -2 /home/ashleyrosb/TrimmedShort/10_S10_reverse_paired.fq.gz -l /home/ashleyrosb/TrimmedLong/MergedLong/mergedbc10.fastq.gz -o /scratch/ashleyrosb/Unicyc_out/uni_bc10
+
+#11
+unicycler -1 /home/ashleyrosb/TrimmedShort/11_S11_forward_paired.fq.gz -2 /home/ashleyrosb/TrimmedShort/11_S11_reverse_paired.fq.gz -l /home/ashleyrosb/TrimmedLong/MergedLong/mergedbc11.fastq.gz -o /scratch/ashleyrosb/Unicyc_out/uni_bc11
+
+#12
+unicycler -1 /home/ashleyrosb/TrimmedShort/12_S12_forward_paired.fq.gz -2 /home/ashleyrosb/TrimmedShort/12_S12_reverse_paired.fq.gz -l /home/ashleyrosb/TrimmedLong/MergedLong/mergedbc12.fastq.gz -o /scratch/ashleyrosb/Unicyc_out/uni_bc12
